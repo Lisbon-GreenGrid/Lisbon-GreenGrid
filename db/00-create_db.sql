44b06174feaef4947720627fffb2902a9e27335d
Adebola <adebolaadedayo21@gmail.com>
2026-02-09 15:16:58 +0000

    Database created

---
+++ db/00-create_db.sql
@@ -0,0 +1,10 @@
+-- Database: lisbon_greengrid
+
+CREATE DATABASE lisbon_greengrid
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LOCALE_PROVIDER = 'libc'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
