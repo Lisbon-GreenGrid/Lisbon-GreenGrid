272fc20aa22f5c3f83d32ad617b25a8d48c0ee12
Adebola <adebolaadedayo21@gmail.com>
2026-02-11 17:13:31 +0000

    trees table for staging area created

---
+++ db/02-create_sa_tables.sql
@@ -0,0 +1,16 @@
+-- Table: sa.trees
+DROP TABLE IF EXISTS sa.trees CASCADE;
+CREATE TABLE IF NOT EXISTS sa.trees
+(
+    tree_id INTEGER PRIMARY KEY,   	   -- Unique ID from the Lisbon City Council dataset
+    nome_vulgar VARCHAR(255),          -- Common name
+    especie VARCHAR(255),              -- Scientific name
+    tipologia VARCHAR(100),            -- Tree type
+    pap NUMERIC(10, 2),                -- Perimeter at breast height (to 2 decimal places)
+	manutencao VARCHAR(100),		   -- Authority in charge of its maintenance
+    ocupacao VARCHAR(100),             -- Occupation
+    local VARCHAR(255),                -- Location
+    morada VARCHAR(255),               -- Address
+    freguesia VARCHAR(100),            -- Parish name
+    geom GEOMETRY(Point, 3763)
+);
