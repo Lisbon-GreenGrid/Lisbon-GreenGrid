2581b9719d1a69bcca692353327e40e430b4bef8
Adebola <adebolaadedayo21@gmail.com>
2026-02-13 18:38:21 +0000

    Trigger created to move sa.trees to pa.trees

--- greengrid_db/05-create_triggers.sql
+++ greengrid_db/05-create_triggers.sql
@@ -1,19 +1,52 @@
---To validate maintenance date
-CREATE OR REPLACE FUNCTION pa.fn_check_maint_date()
+-- To move trees data from sa to pa
+CREATE OR REPLACE FUNCTION sa.insert_trees_in_pa()
 RETURNS TRIGGER
 AS
 $$
 BEGIN
-    IF NEW.maint_date > CURRENT_DATE THEN
-        RAISE EXCEPTION 'Maintenance date cannot be in the future!';
-    END IF;
-    RETURN NEW;
+	INSERT INTO pa.trees (
+	    tree_id,   	   
+	    nome_vulga,          
+	    especie,              
+	    tipologia,            
+	    pap,
+		manutencao,
+	    ocupacao,             
+	    local,                
+	    morada,               
+	    freguesia,            
+	    geometry
+	)
+	SELECT 
+	    tree_id,   	   
+	    nome_vulga,          
+	    especie,              
+	    tipologia,            
+	    pap,
+		manutencao,
+	    ocupacao,             
+	    local,                
+	    morada,               
+	    freguesia,            
+	    geometry
+	FROM sa.trees sa
+	WHERE NOT EXISTS(SELECT 1 FROM pa.trees pa WHERE pa.tree_id = sa.tree_id);
+	
+	
+	UPDATE pa.trees AS pa SET
+		pap = sa.pap,
+		manutencao = sa.manutencao,
+		ocupacao = sa.ocupacao
+	FROM sa.trees AS sa
+	WHERE sa.tree_id = pa.tree_id
+		AND (pa.pap != sa.pap  OR pa.manutencao != sa.manutencao OR pa.ocupacao != sa.ocupacao);
+	RETURN NULL;
 END;
 $$
 LANGUAGE plpgsql;
 
-CREATE OR REPLACE TRIGGER trg_validate_maint_date
-BEFORE INSERT
-ON pa.maintenance
-FOR EACH ROW
-EXECUTE FUNCTION pa.fn_check_maint_date();
+CREATE OR REPLACE TRIGGER insert_trees_in_pa
+AFTER INSERT ON sa.trees
+FOR EACH STATEMENT 
+EXECUTE PROCEDURE sa.insert_trees_in_pa();
+
