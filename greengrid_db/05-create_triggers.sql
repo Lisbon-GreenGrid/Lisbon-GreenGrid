ed6b973f12ff2e108f80f1685cb6e49938dc10f9
Adebola Adedayo <adebolaadedayo21@gmail.com>
2026-02-17 20:35:23 +0000

    Add files via upload

--- greengrid_db/05-create_triggers.sql
+++ greengrid_db/05-create_triggers.sql
@@ -1,52 +1,45 @@
 -- To move trees data from sa to pa
-CREATE OR REPLACE FUNCTION sa.insert_trees_in_pa()
+CREATE OR REPLACE FUNCTION sa.insert_trees_in_pa() -- starts the operation in the staging area
 RETURNS TRIGGER
 AS
 $$
 BEGIN
-	INSERT INTO pa.trees (
-	    tree_id,   	   
-	    nome_vulga,          
-	    especie,              
-	    tipologia,            
-	    pap,
-		manutencao,
-	    ocupacao,             
-	    local,                
-	    morada,               
-	    freguesia,            
-	    geometry
-	)
-	SELECT 
-	    tree_id,   	   
-	    nome_vulga,          
-	    especie,              
-	    tipologia,            
-	    pap,
-		manutencao,
-	    ocupacao,             
-	    local,                
-	    morada,               
-	    freguesia,            
-	    geometry
-	FROM sa.trees sa
-	WHERE NOT EXISTS(SELECT 1 FROM pa.trees pa WHERE pa.tree_id = sa.tree_id);
-	
-	
-	UPDATE pa.trees AS pa SET
-		pap = sa.pap,
-		manutencao = sa.manutencao,
-		ocupacao = sa.ocupacao
-	FROM sa.trees AS sa
-	WHERE sa.tree_id = pa.tree_id
-		AND (pa.pap != sa.pap  OR pa.manutencao != sa.manutencao OR pa.ocupacao != sa.ocupacao);
-	RETURN NULL;
+	-- Check if it exists in production
+    IF NOT EXISTS (SELECT 1 FROM pa.trees WHERE tree_id = new.tree_id) THEN
+		INSERT INTO pa.trees (
+	    	tree_id, nome_vulga, especie, tipologia, pap,
+			manutencao, ocupacao, local, morada, freguesia, geometry
+		)
+		VALUES (
+			new.tree_id,   	   
+	    	new.nome_vulga,          
+	    	new.especie,              
+	    	new.tipologia,            
+	    	new.pap,
+			new.manutencao,
+	    	new.ocupacao,             
+	    	new.local,                
+	    	new.morada,               
+	    	new.freguesia,            
+	    	new.geometry
+		);
+	ELSE
+		-- If it exists, update the record
+		UPDATE pa.trees SET
+        	pap = new.pap,
+            manutencao = new.manutencao,
+            ocupacao = new.ocupacao
+        WHERE tree_id = new.tree_id;
+	END IF;
+	RETURN NEW;
 END;
 $$
 LANGUAGE plpgsql;
 
-CREATE OR REPLACE TRIGGER insert_trees_in_pa
+DROP TRIGGER IF EXISTS insert_trees_in_pa ON sa.trees;
+
+CREATE TRIGGER insert_trees_in_pa
 AFTER INSERT ON sa.trees
-FOR EACH STATEMENT 
-EXECUTE PROCEDURE sa.insert_trees_in_pa();
+FOR EACH ROW 
+EXECUTE FUNCTION sa.insert_trees_in_pa();
 
