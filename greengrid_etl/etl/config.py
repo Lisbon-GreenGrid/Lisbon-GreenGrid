e67e72aa4354b25dad7de7d77e591605f5de9f16
Christian Oluoma <100550816+OceeDiTT@users.noreply.github.com>
2026-02-23 16:13:33 +0000

    Improve docstring for read_config function
    Enhance docstring for read_config function to clarify its purpose, arguments, return value, and exceptions.

--- greengrid_etl/etl/config.py
+++ greengrid_etl/etl/config.py
@@ -1,15 +1,25 @@
+# Import required modules and packages
 import yaml
 from .logs import die
 
 
 def read_config(fname: str) -> dict:
-    """ Reads the configuration file
+    """
+    Load a YAML configuration file and return its contents as a dictionary.
+
+    This function reads the specified YAML file and parses its contents.
+    If the file cannot be parsed correctly, the program exits using the
+    `die` function from the logging module.
 
     Args:
-        fname (str): the configuration file
+        fname (str): Path to the YAML configuration file.
 
     Returns:
-        dict: a dictionary with the configuration
+        dict: Dictionary containing the configuration parameters from the YAML file.
+
+    Raises:
+        SystemExit: If there is an error parsing the YAML file, `die` is called and
+        the program exits.
     """
     try:
         with open(fname) as f:
